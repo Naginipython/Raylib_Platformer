fe03aa75435b5cdb1b1f2129cecc4d6ee2cb16a1
Naginipython <naginipython332@gmail.com>
2024-12-11 13:32:41 -0500

    Added code for a friend to test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "sprites.h"
 
 int main(int argc, char* argv[]) {
-  InitWindow(WORLD_WIDTH*ZOOM, WORLD_HEIGHT*ZOOM, "Celeste Clone");
+  // InitWindow(WORLD_WIDTH*ZOOM, WORLD_HEIGHT*ZOOM, "Celeste Clone");
+  InitWindow(150, 150, "Celeste Clone");
   SetTargetFPS(60);
   Color bgColor = {119, 33, 111, 255};
   GameState gameState;
@@ -13,15 +14,16 @@ int main(int argc, char* argv[]) {
   camera.target = { 0, 0 };
   camera.offset = { 0, 0 };
   camera.rotation = 0.0f;
-  camera.zoom = (float)ZOOM;
+  // camera.zoom = (float)ZOOM;
+  camera.zoom = 3.0f;
   
   while (!WindowShouldClose()) {
     gameState.update();
     BeginDrawing();
     ClearBackground(bgColor);
 
-    BeginMode2D(camera);
-      gameState.draw();
+    // BeginMode2D(camera);
+      // gameState.draw();
       // DrawText("Celeste Clone", 10, 10, 20, DARKGRAY);
       // for (int i = 0; i < WORLD_WIDTH/TILESIZE -1; i++) {
       //   unsigned char temp = 255-i*5;
@@ -30,7 +32,27 @@ int main(int argc, char* argv[]) {
       // DrawRectangle(5*TILESIZE, 10*TILESIZE, TILESIZE, TILESIZE, {255, 255, 255, 255});
 
       // DrawCircle(gameState.player.pos.x, gameState.player.pos.y, gameState.player.size, RED);
-    EndMode2D();
+    // EndMode2D();
+
+    // temp
+    // BeginMode2D(camera);
+    // int i = 0;
+    //   for (int i = 0; i < 10; i++) {
+    //     for (int j = 0; j < 10; j++) {
+    //       DrawRectangle(i*10, j*10, 10, 10, {255, 255, 255, 255});
+    //     }
+    //   }
+    // EndMode2D();
+    unsigned char step = 255/9;
+    int addStep = 0;
+    for (int i = 0; i < 150; i = i+50) {
+      for (int j = 0; j < 150; j = j+50) {
+        unsigned char temp = step*addStep;
+        DrawRectangle(j, i, 50, 50, {temp, temp, temp, 255});
+        addStep++;
+      }
+    }
+
     EndDrawing();
   }
   
